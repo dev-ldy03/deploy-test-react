fc8cd31ae6b0375484d2567c04fcc98386781b05
Dayoung Lee <ldy@Dayoungui-MacBookAir.local>
2024-07-05 21:22:58 +0900

    최초 리액트

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
 
 function App() {
+
+  useEffect(() => {
+    // {process.env.REACT_APP_BACK_END_URL}
+     
+      const getDatas  = async ()=>{
+      const response = await fetch("https://port-0-deploy-test-ly8jl4snb8401bb5.sel5.cloudtype.app/",{
+          method : "POST"
+      });
+      const datas = response.json();
+      return datas;
+    }
+
+    getDatas().then(console.log).catch(console.error);
+  },[])
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
